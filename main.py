02d8bb3b410fe9411a75ea814123e0bba123b5f5
Sitam <sitammeur@gmail.com>
2024-01-27 02:32:57 +0530

    initial commit

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import os
 import google.generativeai as genai
 
 # Set Google API key
-os.environ['GOOGLE_API_KEY'] = "YOUR KEY"
-genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+os.environ["GOOGLE_API_KEY"] = "YOUR KEY"
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 # Create the Model
-txt_model = genai.GenerativeModel('gemini-pro')
-vis_model = genai.GenerativeModel('gemini-pro-vision')
+txt_model = genai.GenerativeModel("gemini-pro")
+vis_model = genai.GenerativeModel("gemini-pro-vision")
 
 sample_prompt = """You are a medical practitioner and an expert in analyzing medical-related images working for a very reputed hospital. You will be provided with images and you need to identify the anomalies, any disease or health issues. You need to generate the result in a detailed manner. Write all the findings, next steps, recommendation, etc. You only need to respond if the image is related to a human body and health issues. You must have to answer but also write a disclaimer saying that "Consult with a Doctor before making any decisions."
 
@@ -19,11 +19,13 @@ Remember, if certain aspects are not clear from the image, it's okay to state 'U
 
 Now analyze the image and answer the above questions in the same structured manner defined above."""
 
+
 # Image to Base 64 Converter
 def image_to_base64(image_path):
-    with open(image_path, 'rb') as img:
+    with open(image_path, "rb") as img:
         encoded_string = base64.b64encode(img.read())
-    return encoded_string.decode('utf-8')
+    return encoded_string.decode("utf-8")
+
 
 # Function that takes User Inputs and displays it on ChatUI
 def query_message(history, txt, img):
@@ -35,25 +37,87 @@ def query_message(history, txt, img):
     history += [(f"{txt} ![]({data_url})", None)]
     return history
 
+
 # Function that takes User Inputs, generates Response and displays on Chat UI
 def llm_response(history, text, img):
     health_keywords = [
-    "health", "disease", "anomaly", "medical", "findings", "recommendation",
-    "wellness", "fitness", "nutrition", "exercise", "diet", "weight loss",
-    "mental health", "physical health", "healthy lifestyle", "preventive care",
-    "symptoms", "treatment", "medicine", "vaccination", "immunization", "stress",
-    "sleep", "hygiene", "well-being", "public health", "healthcare", "doctor",
-    "hospital", "prescription", "pharmacy", "alternative medicine", "holistic health",
-    "chronic illness", "acute illness", "allergies", "asthma", "diabetes",
-    "hypertension", "cardiovascular health", "respiratory health", "digestive health",
-    "orthopedics", "mental disorders", "cognitive health", "aging", "pediatrics",
-    "women's health", "men's health", "reproductive health", "pregnancy", "childbirth",
-    "aging gracefully",
-    "brain", "heart", "lungs", "liver", "kidneys","kidney", "stomach", "intestines",
-    "pancreas", "spleen", "gallbladder", "bladder", "skin", "eyes", "ears", "nose",
-    "mouth", "tongue", "throat", "esophagus", "muscles", "bones", "joints"
-]
-
+        "health",
+        "disease",
+        "anomaly",
+        "medical",
+        "findings",
+        "recommendation",
+        "wellness",
+        "fitness",
+        "nutrition",
+        "exercise",
+        "diet",
+        "weight loss",
+        "mental health",
+        "physical health",
+        "healthy lifestyle",
+        "preventive care",
+        "symptoms",
+        "treatment",
+        "medicine",
+        "vaccination",
+        "immunization",
+        "stress",
+        "sleep",
+        "hygiene",
+        "well-being",
+        "public health",
+        "healthcare",
+        "doctor",
+        "hospital",
+        "prescription",
+        "pharmacy",
+        "alternative medicine",
+        "holistic health",
+        "chronic illness",
+        "acute illness",
+        "allergies",
+        "asthma",
+        "diabetes",
+        "hypertension",
+        "cardiovascular health",
+        "respiratory health",
+        "digestive health",
+        "orthopedics",
+        "mental disorders",
+        "cognitive health",
+        "aging",
+        "pediatrics",
+        "women's health",
+        "men's health",
+        "reproductive health",
+        "pregnancy",
+        "childbirth",
+        "aging gracefully",
+        "brain",
+        "heart",
+        "lungs",
+        "liver",
+        "kidneys",
+        "kidney",
+        "stomach",
+        "intestines",
+        "pancreas",
+        "spleen",
+        "gallbladder",
+        "bladder",
+        "skin",
+        "eyes",
+        "ears",
+        "nose",
+        "mouth",
+        "tongue",
+        "throat",
+        "esophagus",
+        "muscles",
+        "bones",
+        "joints",
+    ]
 
     if not img:
         if any(keyword in text.lower() for keyword in health_keywords):
@@ -67,31 +131,30 @@ def llm_response(history, text, img):
             response = vis_model.generate_content([text, img])
             history += [(None, response.text)]
         else:
-            history += [(None, "I can only respond to health-related questions based on images.")]
+            history += [
+                (
+                    None,
+                    "I can only respond to health-related questions based on images.",
+                )
+            ]
 
     return history
+
 
 # Interface Code
 with gr.Blocks() as app:
     with gr.Row():
         image_box = gr.Image(type="filepath")
 
-        chatbot = gr.Chatbot(
-            scale=2,
-            height=750
-        )
+        chatbot = gr.Chatbot(scale=2, height=750)
     text_box = gr.Textbox(
         placeholder="Enter text and press enter, or upload an image",
         container=False,
     )
 
     btn = gr.Button("Submit")
-    clicked = btn.click(query_message,
-                        [chatbot, text_box, image_box],
-                        chatbot
-                        ).then(llm_response,
-                                [chatbot, text_box, image_box],
-                                chatbot
-                                )
+    clicked = btn.click(query_message, [chatbot, text_box, image_box], chatbot).then(
+        llm_response, [chatbot, text_box, image_box], chatbot
+    )
 app.queue()
 app.launch(share=True)
